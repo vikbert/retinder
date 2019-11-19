245985ad5cdb966e25e29aa1c85f8457663b5708
Xun Zhou <segentor@gmail.com>
2019-11-19 22:08:13 +0100

    updates

--- src/views/components/CardForm.js
+++ src/views/components/CardForm.js
@@ -12,6 +12,15 @@ const initialState = {
 const CardForm = ({closeModal = null}) => {
     const dispatch = useDispatch();
     const [formData, setFormData] = useState(initialState);
+    const [visible, setVisible] = useState(false);
+
+    const SuccessMessage = () => {
+        return (
+            <span className="is-size-7-mobile has-text-success">
+                Saved ✔︎
+            </span>
+        );
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -19,8 +28,13 @@ const CardForm = ({closeModal = null}) => {
             id: uuid(),
             ...formData,
         }));
-        closeModal();
+
         e.currentTarget.reset();
+        setVisible(true);
+
+        window.setTimeout(() => {
+            setVisible(false);
+        }, 3000);
     };
 
     const handleChange = (e) => {
@@ -31,6 +45,17 @@ const CardForm = ({closeModal = null}) => {
 
     return (
         <form className="box is-fullwidth" onSubmit={handleSubmit}>
+            <div className="container">
+                <div className="columns is-mobile">
+                    <div className="column"></div>
+                    <div className="column has-text-centered">
+                        <h3 className="is-2 is-uppercase">Add new card</h3>
+                    </div>
+                    <div className="column has-text-right">
+                        {visible && <SuccessMessage/>}
+                    </div>
+                </div>
+            </div>
             <div className="field">
                 <label htmlFor={'category'} className="label has-text-left">Category</label>
                 <div className="control is-expanded">
@@ -71,7 +96,7 @@ const CardForm = ({closeModal = null}) => {
             </div>
             <div className="field ">
                 <div className="control is-centered">
-                    <button className="button is-success is-fullwidth" type="submit">
+                    <button className="button is-primary is-fullwidth" type="submit">
                         Submit
                     </button>
                 </div>
