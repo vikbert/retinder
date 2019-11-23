eb562427937b0a5e53eafb96602ea73903ae379f
vikbert <segentor@gmail.com>
2019-11-23 13:47:04 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import Login from "./views/user/Login";
 import PrivateRoute from "./routing/PrivateRoute";
 import CardIndex from "./views/card/list";
 import CategoryIndex from "./views/category/list";
+import { home, login, category, card, review } from "./routing/routes";
 
 const App = () => {
   return (
     <Router history={history} basename={"/"}>
       <Switch>
-        <Route exact path={"/"} component={Login} />
-        <Route exact path={"/login"} component={Login} />
-        <PrivateRoute exact path={"/review"} component={StartReview} />
-        <PrivateRoute exact path={"/category"} component={CategoryIndex} />
-        <PrivateRoute exact path={"/card"} component={CardIndex} />
+        <Route exact path={home} component={Login} />
+        <Route exact path={login} component={Login} />
+        <PrivateRoute exact path={category} component={CategoryIndex} />
+        <PrivateRoute exact path={card} component={CardIndex} />
+        <PrivateRoute exact path={review} component={StartReview} />
         <Route component={NotFound} />
       </Switch>
     </Router>
---
+++ src/routing/routes.js
@@ -0,0 +1,5 @@
+export const home = "/";
+export const login = "/login";
+export const category = "/category";
+export const card = "/card/:categoryId";
+export const review = "/review";
