a5354aff4ae298bee5101958727277b82e4cc20e
Xun Zhou <segentor@gmail.com>
2019-11-28 21:13:00 +0100

    update uuidupdate uuidupdate uuidupdate uuid

--- src/utils/UUID.js
+++ src/utils/UUID.js
@@ -1,5 +1,11 @@
+const uuidv3 = require('uuid/v3');
 const crypto = require("crypto");
 
-export default function uuid() {
+export function uuid() {
     return crypto.randomBytes(16).toString("hex");
+}
+
+export function uuid3(username) {
+    const MY_NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
+    return uuidv3(username, MY_NAMESPACE);
 }
