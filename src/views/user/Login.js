776862fd576312aeb1bd1ebc9cfd545adcb925f6
Xun Zhou <segentor@gmail.com>
2019-12-15 12:09:59 +0100

    updates

--- src/views/user/Login.js
+++ src/views/user/Login.js
@@ -1,69 +1,94 @@
 import React from "react";
-import {useDispatch} from "react-redux";
-import {login} from "../../stores/userWidget";
-import {uuid3} from "../../utils/UUID";
-import LogoImage from "../../assets/images/logo.jpg";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { login } from "../../stores/userWidget";
+import { uuid3 } from "../../utils/UUID";
 
-const Login = ({history}) => {
-    const dispatch = useDispatch();
+const CenteredLogin = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  height: 70vh;
+`;
+const Button = styled.button`
+  width: 100%;
+  box-shadow: 0px 1px 0px 1px #fff6af;
+  background: linear-gradient(to bottom, #ffec64 5%, #ffab23 100%);
+  background-color: #ffec64;
+  border-radius: 6px;
+  border: 1px solid #ffbb00;
+  display: inline-block;
+  cursor: pointer;
+  color: #333333;
+  font-size: 18px;
+  font-weight: 500;
+  padding: 10px 24px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #ffee66;
+  :hover {
+    background: linear-gradient(to bottom, #ffab23 5%, #ffec64 100%);
+    background-color: #ffab23;
+  }
+  :active {
+    position: relative;
+    top: 1px;
+  }
+  :focus {
+    outline: none;
+  }
+`;
+const Input = styled.input`
+  margin: 14px 0;
+  padding: 8px;
+  font-size: 18px;
+  border: 1px solid #dedede;
+  :focus {
+    outline: none;
+    border: 1px solid #ffab23;
+  }
+`;
 
-    const handleSubmit = e => {
-        const formData = new FormData(e.currentTarget);
-        dispatch(
-            login({
-                username: formData.get("username"),
-                token: uuid3(formData.get("username")),
-            }),
-        );
-        history.push("/categories");
-    };
+const Label = styled.label`
+  color: transparent;
+  background-color: #565656;
+  text-shadow: 2px 2px 3px rgba(255, 255, 255, 0.5);
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  background-clip: text;
+`;
 
-    const LoginForm = () => (
-        <form className="box" onSubmit={handleSubmit}>
-            <div className="field has-text-centered">
-                <label htmlFor="username" className="label">
-                    Enter User Name
-                </label>
-                <div className="control">
-                    <input
-                        type="text"
-                        className="input"
-                        name={"username"}
-                        required
-                        minLength={3}
-                    />
-                </div>
-            </div>
-            <div className="field">
-                <div className="control">
-                    <button className="button is-success is-fullwidth" type="submit">
-                        Enter
-                    </button>
-                </div>
-            </div>
+const Login = ({ history }) => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    const formData = new FormData(e.currentTarget);
+    dispatch(
+      login({
+        username: formData.get("username"),
+        token: uuid3(formData.get("username"))
+      })
+    );
+    history.push("/categories");
+  };
+
+  return (
+    <>
+      <CenteredLogin>
+        <form onSubmit={handleSubmit}>
+          <Label htmlFor="username" title="Enter User Name">
+            Enter User Name
+          </Label>
+          <br />
+          <Input type="text" name={"username"} required minLength={3} />
+          <br />
+          <Button type="submit">Enter</Button>
+          <br />
         </form>
-    );
-
-    return (
-        <>
-            <section className="hero is-fullheight">
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="columns is-centered">
-                            <div className="column is-5-tablet is-4-desktop is-3-widescreen">
-                                <img
-                                    src={LogoImage}
-                                    alt="retinder"
-                                    style={{margin: "auto", padding: "40px 20px"}}
-                                />
-                                <LoginForm/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+      </CenteredLogin>
+    </>
+  );
 };
 
 export default Login;
